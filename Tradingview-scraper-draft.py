eca04446eea881db6469cedb2d96b9854be20a44
Tong Chen <linnkid.chen@gmail.com>
2025-05-07 17:28:31 +1000

    remove draft file

--- Tradingview-scraper-draft.py
+++
@@ -1,7 +0,0 @@
-# Testing. Grep News from TradingView
-
-from tradingview_scraper.symbols.ideas import Ideas
-# Initialize the Ideas scraper with default parameters
-ideas_scraper = Ideas()  # Default: export_result=False, export_type='json'
-ideas = ideas_scraper.scrape()  # Default symbol: 'BTCUSD'
-print("Ideas:", ideas)
