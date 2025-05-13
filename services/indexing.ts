e27f5b3dc5244636c81f19a68b3269b478a815f3
Tong Chen <linnkid.chen@gmail.com>
2025-05-13 20:00:44 +1000

    chore: add postgres dependency and update podcast indexing logic
    - Added "postgres" dependency to package.json and package-lock.json.
    - Updated podcast indexing logic to retrieve 14 episodes instead of 2.
    - Removed the empty podcast transcription script file.

--- services/indexing.ts
+++ services/indexing.ts
@@ -2,37 +2,37 @@ import { getTranscribedPodcastEpisodes } from './data-retrieve/podcast-data-retr
 import { addDocuments } from './rag';
 const podcastSet = [
   "The Clark Howard Podcast",
-  // "Bloomberg Masters in Business Podcast", 
-  // "Goldman Sachs Exchanges",
-  // "CNBC's Fast Money Podcast",
-  // "Real Vision",
-  // "The Compound and Friends",
-  // "Rational Reminder Podcast",
-  // "M&A Science",
-  // "Australian Finance Podcast (Rask)",
-  // "The Ideas Exchange by ASX",
-  // "We Study Billionaires",
-  // "Motley Fool Money",
-  // "Invest Like the Best",
-  // "Equity Mates Investing Podcast",
-  // "Barron's Streetwise",
-  // "Chat With Traders",
-  // "CNBC's Fast Money",
-  // "The Investing for Beginners Podcast",
-  // "Mad Money w/ Jim Cramer",
-  // "Investing With IBD",
-  // "Wall Street Breakfast – Seeking Alpha",
-  // "The Meb Faber Show",
-  // "WSJ Your Money Briefing",
-  // "ETF Prime",
-  // "Option Alpha Podcast",
-  // "Animal Spirits",
-  // "ETF Spotlight - Zacks",
-  // "Stock Market Today with IBD",
-  // "The Ideas Exchange",
-  // "MarketBeat",
-  // "ETF Spotlight",
-  // "The Best One Yet"
+  "Bloomberg Masters in Business Podcast", 
+  "Goldman Sachs Exchanges",
+  "CNBC's Fast Money Podcast",
+  "Real Vision",
+  "The Compound and Friends",
+  "Rational Reminder Podcast",
+  "M&A Science",
+  "Australian Finance Podcast (Rask)",
+  "The Ideas Exchange by ASX",
+  "We Study Billionaires",
+  "Motley Fool Money",
+  "Invest Like the Best",
+  "Equity Mates Investing Podcast",
+  "Barron's Streetwise",
+  "Chat With Traders",
+  "CNBC's Fast Money",
+  "The Investing for Beginners Podcast",
+  "Mad Money w/ Jim Cramer",
+  "Investing With IBD",
+  "Wall Street Breakfast – Seeking Alpha",
+  "The Meb Faber Show",
+  "WSJ Your Money Briefing",
+  "ETF Prime",
+  "Option Alpha Podcast",
+  "Animal Spirits",
+  "ETF Spotlight - Zacks",
+  "Stock Market Today with IBD",
+  "The Ideas Exchange",
+  "MarketBeat",
+  "ETF Spotlight",
+  "The Best One Yet"
 ];
 
   const filteredPodcastSet = Array.from(new Set(podcastSet));
@@ -42,7 +42,7 @@ export async function indexPodcastEpisodes() {
   try{
     for (const podcast of filteredPodcastSet) {
       // TODO Remove duplicate of episodes from database
-      const episodesData = await getTranscribedPodcastEpisodes(podcast, 2);
+      const episodesData = await getTranscribedPodcastEpisodes(podcast, 14);
       if (episodesData.length === 0) {
         console.log(`No episodes found for podcast: ${podcast}`);
         continue;
